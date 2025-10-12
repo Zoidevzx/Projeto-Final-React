40ce8470d74587c448d9e931f0fc8fb23aec1c7d
Jean1702 <jean.carlos1@estudante.ifgoiano.edu.br>
2025-10-12 05:13:29 -0300

    continuando home meio da pagina

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Home from '@/components/Home';
 import './globals.css'
+import { useRouter } from "next/navigation";
 
 export default function PageHoe() {
   return <Home />;
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,17 +1,24 @@
+'use client'
 import { User, Search, ShoppingCart, ChevronDown } from 'lucide-react';
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  function IrparaShop(){
+    router.push('/shop')
+  }
   return (
     <>
-      <div className="container grid grid-rows-[1fr_7fr_8fr_5fr]">
-        <div className="flex justify-between">
-          <div className="flex-center ml-[10px] mt-[10px]">
+      <div className="container grid grid-rows-[1fr_10fr_5fr_4fr] ">
+        <div className="flex justify-between mr-[20px] ml-[20px]">
+          <div className="flex-center  mt-[10px]">
             <h1 className='m-[0] '>Urban</h1>
           </div>
 
           <div className="flex justify-between gap-[40px] text-[0.875rem]">
             <div className="grid grid-flow-col gap-[10px] ">
-              <div className="flex-center text-xs "> Home </div>
+              <div className="flex-center "> Home </div>
               <div className="flex-center ">About Us </div>
               <div className="flex-center gap-[3px]">Shop <ChevronDown size={14} /></div>
               <div className="flex-center border-0 gap-[3px]">Blog <ChevronDown size={14} /></div>
@@ -19,7 +26,7 @@ export default function Home() {
               <div className="flex-center">GET PRO</div>
             </div>
 
-            <div className="flex-center mr-[10px] gap-[10px]">
+            <div className="flex-center  gap-[10px]">
               <div>
                 <Search size={14} />
               </div>
@@ -33,7 +40,16 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="shop"></div>
+        <div className="flex relative mr-[20px] ml-[20px]">
+          <div className='flex flex-col justify-center z-1'>
+              <h1 className='m-[0]'>Classic Cotton T-Shirt</h1>
+              <p className='text-[9px] w-[350px]'>Expenence ultimate comfart with or premum cotton t-shrts Perfect for everyday wear, they provide a stylish and relaxed fit for ann acasion</p>
+              <button onClick={IrparaShop} className='bg-[#9F1D1D] rounded-[40px] w-[100px] h-[40px] text-[#ffffff] border-none'>Shop Now</button>
+          </div>
+          <div className='absolute right-[0px] '>
+            <img src="Imagem_Home_Principal.webp" alt=""  className='w-[350px] h-[350px] object-cover '/>
+          </div>
+        </div>
 
         <div className="atributs"></div>
 
