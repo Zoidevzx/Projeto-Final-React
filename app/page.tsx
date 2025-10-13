8ca30854fe3abdcd21ec733999c9256f27b90939
Jean Carlos Pereira Souza <2024108102540017@ifgoiano.local>
2025-10-13 14:26:43 -0300

    ajakjdfkaj

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+import Home from '@/components/Home';
+import './globals.css'
+import { useRouter } from "next/navigation";
+
+export default function PageHoe() {
+  return <Home />;
+}
