5bb249f27cd49afaac99a12e98e8e4c118ee0173
Bruno Gonçalves de Freitas <2024108102540002@ifgoiano.local>
2025-10-15 10:38:35 -0300

    Ajustando algumas partes do Shop

--- components/Shop.jsx
+++ components/Shop.jsx
@@ -4,83 +4,68 @@ import { useRouter } from "next/navigation";
 
 export default function Shop() {
   return (
-    <div className="container grid grid-rows-1fr_10fr_5fr_4fr ">
-      <div className="flex justify-between mr-20px ml-20px">
-        <div className="flex-center  mt-10px">
-          <h1 className='m-0 '>Urban</h1>
+    <div className="flex-center">
+      <div className="container grid grid-rows-[3fr_5fr_4fr]">
+        <div className='flex-center flex-col gap-1.2'>
+          <h1 className='m-0 text-2xl'>Shop-Sidebar</h1>
+          <p className='m-0 text-#D3D3D3 text-sm'>Home / Shop-Sidebar</p>
         </div>
 
-        <div className="flex justify-between gap-40px text-0.875rem">
-          <div className="grid grid-flow-col gap-10px ">
-            <div className="flex-center "> Home </div>
-            <div className="flex-center ">About Us </div>
-            <div className="flex-center gap-3px">Shop <ChevronDown size={14} /></div>
-            <div className="flex-center border-0 gap-3px">Blog <ChevronDown size={14} /></div>
-            <div className="flex-center gap-3px">Page <ChevronDown size={14} /> </div>
-            <div className="flex-center">GET PRO</div>
-          </div>
+        <div className="grid grid-flow-col grid-cols-[1fr_8fr] m-0_5 gap-x-10">
 
-          <div className="flex-center gap-10px">
-            <div>
-              <Search size={14} />
+          <div className='grid grid-flow-row grid-rows-auto gap-5'>
+            <div className='flex h-7.25'>
+              <form>
+                <input type="text" placeholder='Search' className='border-1 border-black p-2 h-10' />
+              </form>
+              <button className='bg-[#704204] p-2 flex items-center justify-center h-10 w-10'>
+                <Search size={14} color='white' />
+              </button>
             </div>
+
             <div>
-              <User size={14} />
+              <h3 className='text-4 underline'>Categories</h3>
+              <p className='text-3'>T-Shirts</p>
+              <p className='text-3'>Hoodies</p>
+              <p className='text-3'>Oversized Tees</p>
+              <p className='text-3'>Graphic Tees</p>
+              <p className='text-3'>Zip-Up Hoodies</p>
             </div>
+
             <div>
-              <ShoppingCart size={14} />
+              <h3 className='text-4 underline '>Tags</h3>
+              <p className='text-3'>Casual Wear</p>
+              <p className='ext-3'>Street Style</p>
+              <p className='text-3'>Oversized Fit</p>
+              <p className='text-3'>Cotton Fabric</p>
+              <p className='text-3'>Trendy Designs</p>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div className='flex-center flex-col gap-5px'>
-        <h1 className='m-0 capitalize'>shop-sidebar</h1>
-        <p className='m-0 capitalize  text-#D3D3D3 text-0.82rem'>home/shop-sidebar</p>
-      </div>
+            <div>
+              <h3 className='text-4 underline'>Filter By Price</h3>
+              <p className='text-3'>Less than $10</p>
+              <p className='text-3'>$10-$20</p>
+              <p className='text-3'>$20-$30</p>
+              <p className='text-3'>$30-$40</p>
+              <p className='ext-3'>$40-$50</p>
+            </div>
 
-      <div className="grid grid-flow-col gap-10px grid-cols-1fr_8fr m-0_20px">
-
-        <div className='grid grid-flow-row grid-rows-auto'>
-          <div className='flex h-30px'>
-            <form>
-              <input type="text" placeholder='Search' className=' pl-10px border-r-0 h-24px' />
-            </form>
-            <button className='border-l-0'>
-              <Search size={18} />
-            </button>
-          </div>
-
-          <div className='gap-3p'>
-            <h3 className='capitalize text-14px underline'>caracteristicas</h3>
-            <p className='capitalize text-11px'>t-shirts</p>
-            <p className='capitalize text-11px'>Hoockes</p>
-            <p className='capitalize text-11px'>aaa aaa</p>
-            <p className='capitalize text-11px'>aaaa</p>
-            <p className='capitalize text-11px'>aaa</p>
           </div>
 
           <div className=''>
-            <h3 className='capitalize text-14px underline '>tags</h3>
-            <p className='capitalize  text-11px'>t-shirts</p>
-            <p className='capitalize  text-11px'>Hoockes</p>
-            <p className='capitalize  text-11px'>aaa aaa</p>
-            <p className='capitalize  text-11px'>aaaa</p>
-            <p className='capitalize  text-11px'>aaa</p>
-          </div>
+            <div className='flex justify-between h-11'>
+              <h2 className='text-zinc-500'>Showing 1-9 of 55 results</h2>
+              <button className='border-1 p-2 border-stone-300'>
+                Default sorting
+              </button>
+            </div>
+            <div>
 
-          <div className=''>
-            <h3 className='capitalize text-14px underline'>filter by price</h3>
-            <p className='capitalize  text-11px'>t-shirts</p>
-            <p className='capitalize text-11px'>Hoockes</p>
-            <p className='capitalize text-11px'>aaa aaa</p>
-            <p className='capitalize text-11px'>aaaa</p>
-            <p className='capitalize text-11px'>aaa</p>
+            </div>
           </div>
-
         </div>
-      </div>
 
+      </div>
     </div>
   );
 }
