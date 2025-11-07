6d21391bf594c26f9681e366d18fa98f5041c88e
Jean1702 <jean.carlos1@estudante.ifgoiano.edu.br>
2025-11-07 05:28:32 -0300

    configurando header

--- components/HeaderPage.jsx
+++ components/HeaderPage.jsx
@@ -1,7 +1,48 @@
+'use client'
 import { User, Search, ShoppingCart, ChevronDown, ChevronRight } from 'lucide-react';
 import Link from 'next/link'
-
+import { useState , useEffect } from 'react';
+import { usePathname } from 'next/navigation';
 export function HeaderPage() {
+    const [openshop, setOpenshop] = useState(false)
+    const [openblog, setOpenblog] = useState(false)
+    const [openpage, setOpenpage] = useState(false)
+
+    const pathname = usePathname()
+
+    const itenshop = [
+        {nome: 'Mordern', link: '/shop'},
+        {nome: 'New', link: '/shop'},
+        {nome: 'Classic', link: '/shop'}
+    ]
+
+    const itensblog = [
+        {nome: 'Ana', link: 'https://github.com/Jean1702'},
+        {nome: 'Bruno', link: 'https://github.com/Zoidevzx'},
+        {nome: 'Jean', link: 'https://github.com/Jean1702'},
+        {nome: 'Joao', link: 'https://github.com/Zoidevzx'}
+    ]
+
+    const itenspage = [
+        {nome: 'Home', link: '/'},
+        {nome: 'About', link: '/aboutus'},
+        {nome: 'Shop', link: '/shop'},
+        {nome: 'Cart', link: '/cart'},
+        
+    ]
+
+    const link = [
+        'http://lattes.cnpq.br/4116708456419800', 
+        'http://lattes.cnpq.br/8727570102457133'
+    ]
+
+    const user = link[Math.floor(Math.random()*2)]
+
+    useEffect(() => {
+      setOpenshop(false)  
+      setOpenblog(false)  
+      setOpenpage(false)  
+    }, [pathname])
 
     return (
         <div className="flex-center">
@@ -9,29 +50,76 @@ export function HeaderPage() {
             <header className="container flex justify-between ">
                 
                 <div className="flex-center">
-                    <h1 className='m-0 p-0 font-bold text-3xl'>Urban</h1>
+                    <h1 className='m-0 p-0 font-bold text-3xl xl:text-5xl'>Urban</h1>
                 </div>
 
                 <div className="flex gap-10 text-xs">
 
-                    <div className="grid grid-flow-col gap-3 ">
+                    <div className="grid grid-flow-col gap-3 xl:text-xl">
                         <div className="flex-center cursor-pointer"> <Link href={'/'}>Home</Link>  </div>
                         <div className="flex-center cursor-pointer"> <Link href={'/aboutus'}>About Us</Link>  </div>
-                        <div  className="flex-center gap-1 cursor-pointer"> <Link href={'/shop'}> Shop</Link> <ChevronDown size={12} /></div>
-                        <div className="flex-center border-0 gap-1 cursor-pointer">Blog <ChevronDown size={12} /></div>
-                        <div className="flex-center gap-1 cursor-pointer">Page<ChevronDown size={12} /> </div>
+                        <div  className="flex-center gap-1 cursor-pointer relative"> 
+                            <Link href={'/shop'}> Shop</Link> 
+                            <ChevronDown size={12} className={'xl:size-4'} onClick={() => openblog ? (setOpenblog(!openblog), setOpenshop(!openshop)) : openpage ? (setOpenpage(!openpage), setOpenshop(!openshop)) : setOpenshop(!openshop)} />
+                            
+                            {openshop && (
+                                <ul className='flex flex-col absolute top-8 gap-1 z-10 p-1 bg-white'>
+                                    {itenshop.map((e, i) =>(
+                                        <li key={i}>
+                                            <Link href={e.link}>
+                                                {e.nome}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                        <div className="flex-center border-0 gap-1 cursor-pointer relative">
+                            <Link href={'https://github.com/Zoidevzx/Projeto-Final-React'}>Blog</Link>
+                            <ChevronDown size={12} className={'xl:size-4'} onClick={() => openshop ? (setOpenshop(!openshop), setOpenblog(!openblog)) : openpage ? (setOpenpage(!openpage), setOpenblog(!openblog)) : setOpenblog(!openblog)} />
+
+                            {openblog && (
+                                <ul className='flex flex-col absolute top-8 left-0 gap-1 z-10 p-1 bg-white'>
+                                    {itensblog.map((e, i) =>(
+                                        <li key={i}>
+                                            <Link href={e.link}>                                            
+                                                {e.nome}
+                                            </Link>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+
+                        </div>
+                        <div className="flex-center gap-1 cursor-pointer relative" onClick={() => openshop ? (setOpenshop(!openshop), setOpenpage(!openpage)): openblog ? (setOpenblog(!openblog), setOpenpage(!openpage)) : setOpenpage(!openpage)}>
+                            Page<ChevronDown size={12} className={'xl:size-4'}/> 
+
+                            {openpage && (
+                                <ul className='flex flex-col absolute top-8 left-0 gap-1 z-10 p-1 bg-white'>
+                                    {itenspage.map((e, i) => (
+                                        <li key={i}>
+                                           <Link href={e.link}>
+                                                {e.nome}
+                                           </Link>                                     
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
                     </div>
 
-                    <div className="flex-center gap-3">
+                    <div className="flex-center gap-3 max-sm:">
                         <div>
-                            <Search size={12} />
+                            <Search size={12} className={'xl:size-5'}/>
                         </div>
                         <div>
-                            <User size={12} />
+                            <Link href={user}>
+                                <User size={12} className={'xl:size-5'} />
+                            </Link>
                         </div>
                         <div >
                             <Link href={'/cart'}>
-                                <ShoppingCart size={12} />
+                                <ShoppingCart size={12} className={'xl:size-5'}/>
                             </Link>
                         </div>
                     </div>
