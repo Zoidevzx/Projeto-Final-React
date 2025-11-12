c4ca4ac62f9886af6ba39eee500f10de64441108
Jean Carlos Pereira Souza <2024108102540017@ifgoiano.local>
2025-11-12 15:04:44 -0300

    adicionando  pasta do backend

---
+++ Backend/table.sql
@@ -0,0 +1,30 @@
+CREATE TABLE product(
+	ID SERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR(255) NOT NULL,
+	price DECIMAl(10,2) NOT NULL,
+	image_url VARCHAR(255) NOT NULL,
+	category VARCHAR(255) NOT NULL,
+	tags VARCHAR (255) not null
+);
+
+CREATE TABLE stock(
+	ID SERIAL PRIMARY KEY,
+	quantitty INT NOT NULL,
+	ID_product INT NOT NULL,
+	FOREIGN KEY(ID_product) REFERENCES product(ID)
+);
+
+CREATE TABLE cart(
+	ID SERIAL PRIMARY KEY,
+	quantity INT NOT NULL,
+	session_id INT NOT NULL
+);
+
+CREATE TABLE sale(
+	ID SERIAL PRIMARY KEY,
+	ID_product INT NOT NULL,
+	FOREIGN KEY(ID_product) REFERENCES product(ID),
+	ID_stock INT NOT NULL,
+	FOREIGN KEY(ID_stock) REFERENCES stock(ID)
+)
